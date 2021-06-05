1e994c83ddb3dedb06059c0c4d2f14eb478e5afb
kadyrbek-python <84332679+kadyrbek-python@users.noreply.github.com>
2021-06-05 20:20:10 +0600

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,21 @@
+class bancAccauont:
+
+    def __init__(self,name, balans):
+        self._name = name
+        self._balans = balans
+
+    @property
+    def name(self):
+        return f"my Accauont:{self._name}"
+    @property
+    def balans(self):
+        if self._balans < 0:
+            print(None)
+        else:
+            return f"my balans:{self._balans}"
+
+Accauont = bancAccauont('kadyrbek', 2000)
+Accauont._balans = 300
+
+print(Accauont.name)
+print(Accauont.balans)
